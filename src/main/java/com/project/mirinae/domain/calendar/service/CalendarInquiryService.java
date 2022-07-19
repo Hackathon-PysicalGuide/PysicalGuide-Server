a6809207f927dc539aacac55ee5b288a6a9718e4
Yongbin <rhdydqls1024@naver.com>
2022-07-19 18:22:32 +0900

    마ㅣㄻ닐ㅇ넌ㅇ러ㅏㄴㅇ마ㅣ럼나ㅣ얼ㄴㅇ머ㅏㅣㄴㅇ

--- src/main/java/com/project/mirinae/domain/calendar/service/CalendarInquiryService.java
+++ src/main/java/com/project/mirinae/domain/calendar/service/CalendarInquiryService.java
@@ -1,4 +1,40 @@
 package com.project.mirinae.domain.calendar.service;
 
+import com.project.mirinae.domain.calendar.entity.Calendar;
+import com.project.mirinae.domain.calendar.exception.CalendarNotFoundException;
+import com.project.mirinae.domain.calendar.presentation.dto.request.CalendarInquiryRequest;
+import com.project.mirinae.domain.calendar.presentation.dto.response.CalendarDataResponse;
+import com.project.mirinae.domain.calendar.presentation.dto.response.CalendarResponse;
+import com.project.mirinae.domain.calendar.repository.CalendarRepository;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class CalendarInquiryService {
+
+    private static final Logger log = LoggerFactory.getLogger(CalendarInquiryService.class);
+    private final CalendarRepository calendarRepository;
+
+    public CalendarResponse execute(CalendarInquiryRequest request) {
+
+        Calendar found = calendarRepository.findById(request.getDate()).orElseThrow(() -> {
+            throw CalendarNotFoundException.EXCEPTION;
+        });
+
+        CalendarDataResponse calendarData = CalendarDataResponse.builder()
+                .category(found.getCategory())
+                .date(found.getDate())
+                .isExercise(found.isExercise())
+                .build();
+
+        log.info("CalendarInquiry Success!!");
+
+        return CalendarResponse.builder()
+                .calendarData(calendarData)
+                .build();
+    }
+
 }
--- src/main/java/com/project/mirinae/domain/calendar/service/CalendarSaveService.java
+++ src/main/java/com/project/mirinae/domain/calendar/service/CalendarSaveService.java
@@ -1,4 +1,50 @@
 package com.project.mirinae.domain.calendar.service;
 
+import com.project.mirinae.domain.calendar.entity.Calendar;
+import com.project.mirinae.domain.calendar.exception.CalendarAlreadyExistsByDateException;
+import com.project.mirinae.domain.calendar.presentation.dto.request.CalendarDataRequest;
+import com.project.mirinae.domain.calendar.presentation.dto.response.CalendarDataResponse;
+import com.project.mirinae.domain.calendar.presentation.dto.response.CalendarResponse;
+import com.project.mirinae.domain.calendar.repository.CalendarRepository;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class CalendarSaveService {
+
+    private static final Logger log = LoggerFactory.getLogger(CalendarSaveService.class);
+    private final CalendarRepository calendarRepository;
+
+    @Transactional
+    public CalendarResponse execute(CalendarDataRequest request) {
+
+        calendarRepository.findById(request.getDate()).ifPresent(m -> {
+            throw CalendarAlreadyExistsByDateException.EXCEPTION;
+        });
+
+        Calendar calendar = Calendar.builder()
+                .category(request.getCategory())
+                .date(request.getDate())
+                .isExercise(request.isExercise())
+                .build();
+
+        calendar = calendarRepository.save(calendar);
+
+        CalendarDataResponse calendarData = CalendarDataResponse.builder()
+                .category(calendar.getCategory())
+                .date(calendar.getDate())
+                .isExercise(calendar.isExercise())
+                .build();
+
+        log.info("CalendarSave Success!!");
+
+        return CalendarResponse.builder()
+                .calendarData(calendarData)
+                .build();
+    }
+
 }
